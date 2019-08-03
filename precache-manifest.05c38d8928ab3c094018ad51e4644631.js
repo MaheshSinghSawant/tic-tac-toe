cff8b2488f8184788c4b00d297f428765e521f0e
Mahesh Singh Sawant <msawant@mail.sfsu.edu>
2019-08-02 19:06:21 -0700

    Updates

---
+++ precache-manifest.05c38d8928ab3c094018ad51e4644631.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4dd1d33a5bf7d9f964a8daa6b5c81a",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "3523a00a4899ea220d82",
+    "url": "/tic-tac-toe/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/tic-tac-toe/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "3523a00a4899ea220d82",
+    "url": "/tic-tac-toe/static/js/main.0b950fa4.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic-tac-toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
